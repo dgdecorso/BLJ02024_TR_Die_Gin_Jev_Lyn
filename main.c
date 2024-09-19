71e62ece5a001a29806f6076716099057c9cf796
dgdecorso <diego.decorso@edu.tbz.ch>
2024-09-19 13:37:40 +0200

    many things

--- main.c
+++ main.c
@@ -6,12 +6,16 @@
 
 int main(void)
 
-int num;
-int num2;
+
 {
+
+    int num;
+    int num2;
+
+
     printf("Enter Nonmbre!\n");
     scanf("%d", &num);
     scanf("%d", &num2);
-    printf("%d + %d = %d") num, num2, num + num2;
+    printf("%d + %d = %d"); num, num2, num + num2;
     return 0;
 }
