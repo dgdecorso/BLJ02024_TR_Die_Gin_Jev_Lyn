819e5c1974e486b4679163df6a3c0e1b024e106e
jevshyl <jevgenia.shylovich@edu.tbz.ch>
2024-09-19 16:13:51 +0200

    Update wurzeln.c

--- wurzeln.c
+++ wurzeln.c
@@ -1,3 +1,13 @@
-//
-// Created by zheny on 19/09/2024.
-//
+#include "stdio.h"
+
+
+void printWurzel() {
+
+double num;
+    double result = 4;
+
+num = result / num;
+
+    printf("The result is %lf\n"), num;
+
+}
