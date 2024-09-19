786861ea0e53dfd1ae5d982bcf2c3375cd1d9b74
dgdecorso <diego.decorso@edu.tbz.ch>
2024-09-19 14:40:05 +0200

    j

--- exponent.c
+++ exponent.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
 void Hoch() {
+    int a;
 
+    printf("hochrechnen yippieeeee\n");
+    scanf("%d",&a);
+    //scanf("%d",&b);
 
-
-
+    printf("%d^%d\n"),a , a, a * a;
 
 
 
