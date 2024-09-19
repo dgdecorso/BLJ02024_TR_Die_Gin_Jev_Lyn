c1fabc19230c6487bebedb8f092b4db2dd41df37
dgdecorso <diego.decorso@edu.tbz.ch>
2024-09-19 13:19:43 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int num;
+int num2;
+
+
+
+int main(void) {
+    printf("Enter Nonmbre!\n");
+    scanf("%d", &num);
+    scanf("%d", &num2);
+    printf("%d + %d = %d"); num, num2, num + num2;
+
+printf("testcnh3   asfasfa ndfjbhj")
+    return 0;
+}
