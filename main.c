4c4d0e543453e8a88d97b4486e46a752500edba2
dgdecorso <diego.decorso@edu.tbz.ch>
2024-09-19 13:55:23 +0200

    test calculator

--- main.c
+++ main.c
@@ -16,6 +16,6 @@ int main(void)
     printf("Enter Nonmbre!\n");
     scanf("%d", &num);
     scanf("%d", &num2);
-    printf("%d + %d = %d"); num, num2, num + num2;
+    printf("%d + %d = %d\n",num, num2, num + num2);
     return 0;
 }
