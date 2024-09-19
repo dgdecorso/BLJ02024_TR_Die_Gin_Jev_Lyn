c366764daaa48474e2872c7a057086b58dbba74e
LynnRuchti <lynn.ruchti@edu.tbz.ch>
2024-09-19 13:53:38 +0200

    changes (all)

---
+++ cmake-build-debug/CMakeFiles/BLJ2024_TR_Die_Gin_Jev_Lyn.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "BLJ2024_TR_Die_Gin_Jev_Lyn.exe"
+  "BLJ2024_TR_Die_Gin_Jev_Lyn.exe.manifest"
+  "BLJ2024_TR_Die_Gin_Jev_Lyn.pdb"
+  "CMakeFiles/BLJ2024_TR_Die_Gin_Jev_Lyn.dir/main.c.obj"
+  "CMakeFiles/BLJ2024_TR_Die_Gin_Jev_Lyn.dir/main.c.obj.d"
+  "libBLJ2024_TR_Die_Gin_Jev_Lyn.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BLJ2024_TR_Die_Gin_Jev_Lyn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
