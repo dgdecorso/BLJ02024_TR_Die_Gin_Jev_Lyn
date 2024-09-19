0192ed3044ab9c22232b0538f1c8c30ddcf81e71
LynnRuchti <lynn.ruchti@edu.tbz.ch>
2024-09-19 16:12:27 +0200

    added fakultaet

--- fakultaet.c
+++ fakultaet.c
@@ -1,3 +1,28 @@
+#include <stdio.h>
 //
 // Created by dgdec on 19.09.2024.
 //
+
+
+
+
+int num();
+
+int fakultaet1() {
+    int number;
+    int result;
+
+    result = num(number);
+
+    return result;
+}
+
+int num(int number) {
+    int result = 1;
+    while (number != 0) {
+
+        result = result * number;
+        number--;
+    }
+    return result;
+}
---
+++ fakultaet.h
@@ -0,0 +1,8 @@
+//
+// Created by liaor on 19.09.2024.
+//
+
+#ifndef FAKULTAET_H
+#define FAKULTAET_H
+
+#endif FAKULTAET_H
