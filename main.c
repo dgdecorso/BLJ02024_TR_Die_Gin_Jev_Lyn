2dd0a83d6efa852c53e0a18e373907170cb7faed
dgdecorso <diego.decorso@edu.tbz.ch>
2024-09-19 13:26:46 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
+
+
+
+
+int main(void)
+
 int num;
 int num2;
-
-
-
-int main(void) {
+{
     printf("Enter Nonmbre!\n");
     scanf("%d", &num);
     scanf("%d", &num2);
-    printf("%d + %d = %d"); num, num2, num + num2;
-
-printf("test")
+    printf("%d + %d = %d") num, num2, num + num2;
     return 0;
 }
