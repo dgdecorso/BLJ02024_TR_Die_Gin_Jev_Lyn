65818e31e2b253df5ab7d30722979aac2e9d7c71
jevshyl <jevgenia.shylovich@edu.tbz.ch>
2024-09-20 09:41:57 +0200

    .

--- wurzeln.c
+++ wurzeln.c
@@ -1,13 +1,16 @@
-#include "stdio.h"
+#include <stdio.h>
 
 
-void printWurzel() {
 
-double num;
-    double result = 4;
 
-num = result / num;
 
-    printf("The result is %lf\n"), num;
+double printWurzel(double square) {
+
+    double root = square/3;
+    int i;
+    if (square <= 0) return 0;
+    for (i = 0; i < 32 ; i++)
+        root = (root + square / root) / 2;
+    return root;
 
 }
---
+++ wurzeln.h
@@ -0,0 +1,6 @@
+
+
+#ifndef WURZEL_H
+#define WURZEL_H
+double printWurzel(double square);
+#endif //WURZEL_H
