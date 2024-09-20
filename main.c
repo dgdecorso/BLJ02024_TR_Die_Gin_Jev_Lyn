bcb0010730a8ebde8fb0583eebf07b39899c1991
gnowngr <gino.wenger@edu.tbz.ch>
2024-09-20 10:43:09 +0200

    things

--- main.c
+++ main.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-
 #include "exponent.h"
-
-
-
+#include "plusminus.h"
 
 int main(void) {
     hoch();
-
-
+    plusminus();
 
     return 0;
 }
---
+++ plusminus.h
@@ -0,0 +1,8 @@
+//
+// Created by gwenger on 20.09.2024.
+//
+
+#ifndef PLUSMINUS_H
+#define PLUSMINUS_H
+int plusminus()
+#endif //PLUSMINUS_H
