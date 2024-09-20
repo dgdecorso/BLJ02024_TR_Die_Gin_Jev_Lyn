16d210caa0a76b38290b6e99106aa068c982d8c8
dgdecorso <diego.decorso@edu.tbz.ch>
2024-09-20 09:43:17 +0200

    exponent

--- main.c
+++ main.c
@@ -4,8 +4,11 @@
 
 
 
-
 int main(void) {
+printf("********** CALCULATOR **********\n");
+printf("Enter operator 1 for + ,2 for - ,3 for * ,4 for / ,5 for ^ ,6 for root,7 for faculty");
+
+
     hoch();
 
 
