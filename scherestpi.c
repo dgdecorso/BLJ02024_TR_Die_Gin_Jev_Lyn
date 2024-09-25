d5de4358ad3e5026c8e385b2ccb44cb3d311580d
dgdecorso <diego.decorso@edu.tbz.ch>
2024-09-25 10:17:23 +0200

    working rps

--- scherestpi.c
+++ scherestpi.c
@@ -17,8 +17,8 @@ int stpi() {
 
     printf("Please enter a number between 1 and 3 ROCK = 1 PAPER = 2 SCISSORS = 3: \n");
 
-    int random = rand() % (max - min) + min;
-    printf("%d"), random;
+
+    int random = rand() % (max - min + 1) + min;
     scanf("%d", &guess);
 
     if (guess == random) {
@@ -31,5 +31,5 @@ int stpi() {
     if ((guess == 1 && random == 2) || (guess == 2 && random == 3) || (guess == 3 && random == 1)) {
         printf("YOU LOST\n");
     }
-    printf("%d\n"), random;
+    printf("%d\n", random);
 }
