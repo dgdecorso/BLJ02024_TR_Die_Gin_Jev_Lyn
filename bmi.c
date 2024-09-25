95fef563d30d82f63c56516f5593b134510af563
dgdecorso <diego.decorso@edu.tbz.ch>
2024-09-25 13:57:49 +0200

    bmi works again

--- bmi.c
+++ bmi.c
@@ -3,14 +3,13 @@
 //
 
 #include "bmi.h"
-
 #include <stdio.h>
 
 
 void bmi () {
     float weight;
     float height;
-
+    float bmi;
 
     printf("Enter Weight in KG: \n");
 
@@ -18,41 +17,26 @@ void bmi () {
 
     printf("Enter Height in Meters: \n");
     scanf("%f", &height);
-
-    printf("Your BMI score: %f\n", weight / (height * height));
-
-
-    float bmi() {
-        float weight;
-        float height;
-        float bmi;
+    bmi = weight / (height * height);
+    printf("Your BMI score: %f\n", bmi);
 
 
-        printf("Enter Weight in KG: \n");
-
-        scanf("%f", &weight);
-
-        printf("Enter Height in Meters: \n");
-        scanf("%f", &height);
-        bmi = (weight / (height * height));
-        printf("Your BMI score: %f\n\n", bmi);
-
-        if (bmi < 18.5) {
-            printf("You're Underweight\n");
-        }
-        else if (bmi >= 18.5 && bmi <= 24.9) {
-            printf("You're Normal\n");
-        }
-        else if (bmi >= 25 && bmi <= 29.9) {
-            printf("You're Overweight\n");
-        }
-        else if (bmi >= 30 && bmi <= 39.9) {
-            printf("You're Obese\n");
-        }
-        else if (bmi >= 40) {
-            printf("You're Morbidly Obese\n");
-        }
-
-        printf("\n\n<18.5 \t\tUnderweight\n18.5-24.9 \tNormal\n25.0-29.9 \tOverweight\n30.0-39.9 \tObese\n>40 \t  \tMorbidly\n\n");
+    if (bmi < 18.5) {
+        printf("You're Underweight\n");
     }
+    else if (bmi >= 18.5 && bmi <= 24.9) {
+        printf("You're Normal\n");
+    }
+    else if (bmi >= 25 && bmi <= 29.9) {
+        printf("You're Overweight\n");
+    }
+    else if (bmi >= 30 && bmi <= 39.9) {
+        printf("You're Obese\n");
+    }
+    else if (bmi >= 40) {
+        printf("You're Morbidly Obese\n");
+    }
+
+    printf("\n\n<18.5 \t\tUnderweight\n18.5-24.9 \tNormal\n25.0-29.9 \tOverweight\n30.0-39.9 \tObese\n>40 \t  \tMorbidly\n\n");
 }
+
